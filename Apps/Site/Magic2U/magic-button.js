b155b6fa3843f6d0e68d4fa1f7d17bf1af7a1bab
Health 3 the Earth <1akieckhefer@gmail.com>
2026-02-12 23:18:05 -0800

    Update magic-button.js

--- Apps/Site/Magic2U/magic-button.js
+++ Apps/Site/Magic2U/magic-button.js
@@ -1,1 +1,13 @@
+class MagicButton extends HTMLElement {
+  connectedCallback() {
+    this.style.display = "inline-block";
+    this.style.padding = "12px 20px";
+    this.style.background = "var(--magic-primary)";
+    this.style.color = "white";
+    this.style.borderRadius = "var(--magic-radius)";
+    this.style.cursor = "pointer";
+    this.style.fontWeight = "bold";
+  }
+}
 
+customElements.define("magic-button", MagicButton);
