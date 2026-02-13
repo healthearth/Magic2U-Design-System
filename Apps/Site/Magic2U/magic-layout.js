671735739428225935b067f33cfdcd3c3c0fcd02
Health 3 the Earth <1akieckhefer@gmail.com>
2026-02-12 23:18:31 -0800

    Update magic-layout.js

--- Apps/Site/Magic2U/magic-layout.js
+++ Apps/Site/Magic2U/magic-layout.js
@@ -1,1 +1,12 @@
+class MagicLayout extends HTMLElement {
+  connectedCallback() {
+    this.style.display = "flex";
+    this.style.justifyContent = "center";
+    this.style.alignItems = "center";
+    this.style.height = "100vh";
+    this.style.background = "linear-gradient(135deg, #0f0e17, #1b1a2e)";
+  }
+}
+
+customElements.define("magic-layout", MagicLayout);
 
