37fead5888a9a10148ab1fd8923d8e43fa85a19b
Health 3 the Earth <1akieckhefer@gmail.com>
2026-02-16 08:55:03 -0800

    Update GovernanaceModel.tsx

--- apps/docs-site/governance/GovernanaceModel.tsx
+++ apps/docs-site/governance/GovernanaceModel.tsx
@@ -1,1 +1,14 @@
+export function GovernanceModel() {
+  return (
+    <div>
+      <h2>Design System Governance</h2>
+      <ul>
+        <li>Component Review Board</li>
+        <li>Accessibility Enforcement</li>
+        <li>Token Change Approval</li>
+        <li>Quarterly Design Audit</li>
+      </ul>
+    </div>
+  );
+}
 
