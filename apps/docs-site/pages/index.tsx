298db98e731e5238a0a2eea79c8427d2f87ba43e
Health 3 the Earth <1akieckhefer@gmail.com>
2026-02-16 08:55:57 -0800

    Update index.tsx

--- apps/docs-site/pages/index.tsx
+++ apps/docs-site/pages/index.tsx
@@ -1,1 +1,12 @@
+import { DocLayout } from "../components/DocLayout";
+import { AdoptionGraph } from "../adoption-graphs/AdoptionGraph";
+
+export default function Home() {
+  return (
+    <DocLayout>
+      <h1>Magic2U Design System</h1>
+      <AdoptionGraph />
+    </DocLayout>
+  );
+}
 
