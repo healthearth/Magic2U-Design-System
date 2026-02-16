d827787e98b95864288cc6ed05a7ad771f62e16b
Health 3 the Earth <1akieckhefer@gmail.com>
2026-02-16 08:54:42 -0800

    Update CodeBlock.tsx

--- apps/docs-site/components/CodeBlock.tsx
+++ apps/docs-site/components/CodeBlock.tsx
@@ -1,1 +1,13 @@
+export function CodeBlock({ code }: { code: string }) {
+  return (
+    <pre style={{
+      background: "#0f172a",
+      padding: 20,
+      borderRadius: 8,
+      color: "#38bdf8"
+    }}>
+      {code}
+    </pre>
+  );
+}
 
