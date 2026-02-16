1c2e41c17bbaac819f0c752ecd2e0adf183f6976
Health 3 the Earth <1akieckhefer@gmail.com>
2026-02-16 09:38:19 -0800

    Update axe-runner.ts

--- packages/accessibility/axe-runner.ts
+++ packages/accessibility/axe-runner.ts
@@ -1,6 +1,11 @@
-import axe from "axe-core";
+import axe from 'axe-core';
+import { JSDOM } from 'jsdom';
 
-export async function runAxeAudit(element: HTMLElement) {
-  return await axe.run(element);
+export async function runAxe(html: string) {
+  const dom = new JSDOM(html);
+  const { window } = dom;
+
+  const results = await axe.run(window.document);
+
+  return results.violations;
 }
-
