62e63bc43fe317e0ffdd5beca995d5f37de7e50c
Health 3 the Earth <1akieckhefer@gmail.com>
2026-02-16 09:47:59 -0800

    Update magic-card.ts

--- packages/ui/web-components/magic-card.ts
+++ packages/ui/web-components/magic-card.ts
@@ -1,1 +1,12 @@
+class MagicCard extends HTMLElement {
+  connectedCallback() {
+    this.innerHTML = `
+      <div style="padding:16px;border:1px solid #ddd">
+        ${this.innerHTML}
+      </div>
+    `;
+  }
+}
+
+customElements.define('magic-card', MagicCard);
 
