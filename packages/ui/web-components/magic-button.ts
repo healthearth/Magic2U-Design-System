b9fd08e83bb732544179ed04f44a64ef2b98b0a2
Health 3 the Earth <1akieckhefer@gmail.com>
2026-02-16 09:47:45 -0800

    Update magic-button.ts

--- packages/ui/web-components/magic-button.ts
+++ packages/ui/web-components/magic-button.ts
@@ -1,1 +1,8 @@
+class MagicButton extends HTMLElement {
+  connectedCallback() {
+    this.innerHTML = `<button>${this.textContent}</button>`;
+  }
+}
+
+customElements.define('magic-button', MagicButton);
 
