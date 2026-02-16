81f76546ff9b4ac2f6eecbce3364f5114fe6293e
Health 3 the Earth <1akieckhefer@gmail.com>
2026-02-16 08:53:28 -0800

    Update AdoptionGraph.tsx

--- apps/docs-site/adoption-graphs/AdoptionGraph.tsx
+++ apps/docs-site/adoption-graphs/AdoptionGraph.tsx
@@ -1,1 +1,11 @@
+export function AdoptionGraph() {
+  return (
+    <div>
+      <h2>Adoption Growth</h2>
+      <p>Year 1: 3 teams</p>
+      <p>Year 2: 12 teams</p>
+      <p>Year 3: 38 teams</p>
+    </div>
+  );
+}
 
