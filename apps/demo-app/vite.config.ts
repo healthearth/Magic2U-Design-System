6a867e5246de56fb2132eaf74cb4ea2cc0d6c30d
Health 3 Earth - A.C.K. = Heal the art hack <1akieckhefer@gmail.com>
2026-02-17 15:14:41 -0800

    Update vite.config.ts

--- apps/demo-app/vite.config.ts
+++ apps/demo-app/vite.config.ts
@@ -1,11 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      input: 'src/main.tsx'
+  resolve: {
+    alias: {
+      'magic2u-design-system': path.resolve(__dirname, '../../packages/ui')
     }
   }
 })
